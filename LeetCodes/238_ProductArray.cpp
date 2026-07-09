ec540294074885da93d66fa04897df8a536b9558
M-Rayasat <onlinee723@gmail.com>
2026-07-09 11:50:00 +0500

    Space Complexity Optimized

--- LeetCodes/238_ProductArray.cpp
+++ LeetCodes/238_ProductArray.cpp
@@ -3,24 +3,23 @@
 using namespace std;
 
 
-// Product of Array of each element except self, by Optimal approach O(n)
+// Product of Array of each element except self, by Optimal approach Time O(n) and space O(1)
 vector<int> product(vector<int> nums){ 
     int n=nums.size();
     vector<int> ans(n,1);
-    vector<int> prefix(n,1);
-    vector<int> suffix(n,1);
 
+    // Prefix => ans
     for(int i=1; i<n; i++){
-        prefix[i]=prefix[i-1]*nums[i-1];
+        ans[i]=ans[i-1]*nums[i-1];
     }
 
+    // Suffix multiply
+    int suffix=1;
     for(int i=n-2; i>=0; i--){
-        suffix[i]=suffix[i+1]*nums[i+1];
+        suffix*=nums[i+1];
+        ans[i]*=suffix;
     }
 
-    for(int i=0; i<n; i++){
-        ans[i]=prefix[i]*suffix[i];
-    }
 
     return ans;
 }
